b3080ec0a837d5046469009ebafacad939c1f045
NoahsarkTaiwan <ben109092ben@gmail.com>
2024-03-20 19:33:00 +0800

    update

--- lib/main.dart
+++ lib/main.dart
@@ -7,47 +7,18 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        backgroundColor: Colors.blue[100],
         appBar: AppBar(
-          title: Text(
-            "JSON File Checker",
-            style: TextStyle(
-              fontWeight: FontWeight.bold,
-              color: Colors.white,
-            ),
-          ),
-          backgroundColor: Colors.blue[200],
-          elevation: 500,
-          leading: Icon(
-            Icons.menu,
-            color: Colors.white,
-          ),
+          title: Text("file checker"),
         ),
         body: Center(
-          child: Container(
-            height: 300,
-            width: 300,
-            decoration: BoxDecoration(
-              color: Colors.blue[200],
-              borderRadius: BorderRadius.circular(20),
-            ),
-            padding: EdgeInsets.all(25),
-            child: Text(
-              "輸入比對資料",
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: 28,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-          ),
+          child: Text("hello world"),
         ),
       ),
     );
